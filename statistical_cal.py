8c9e2a8e00d81b27af0301e732d69ca7a5a75bfd
progacze <patryk_rogaczewski@sevenet.pl>
2019-07-19 14:49:34 +0200

    Added some functions to statistic

--- statistical_cal.py
+++ statistical_cal.py
@@ -1,7 +1,33 @@
-def statistical_cal():
-    print("Age in years: ")
-    print()
-    print()
-    print()
-    print()
-    print()
+class DataStat():
+    def __init__(self):
+
+    def menu(self):
+        print("1. #age [Age in years]: ")
+        print("2. #sex [1 = male; 0 = female]: ")
+        print("3. #cp [chest pain type]: ")
+        print("4. #trestbps [resting blood pressure in mm Hg on admission to the hospital]: ")
+        print("5. #chol [serum cholestoral in mg/dl]: ")
+        print("6. #fbs [fasting blood sugar > 120 mg/dl 1 = true & 0 = false]: ")
+        print("7. #restecg [resting electrocardiographic results]: ")
+        print("8. #thalach [maximum heart rate achieved]: ")
+        print("9. #exang [exercise induced angina 1 = yes & 0 = noe]: ")
+        print("10. #oldpeak [ST depression induced by exercise relative to rest]: ")
+        print("11. #slope [the slope of the peak exercise ST segment]: ")
+        print("12. #ca [number of major vessels 0-3 colored by fluorosopy]: ")
+        print("13. #thal [3= normal; 6 = fixed defect; 7 = reversable defect]: ")
+        print("14. #target [1 or 0]: ")
+
+    def average(self):
+        #Średnia dla wybranej kolumny
+
+    def median(self):
+        #Mediana dla wybranej kolumny
+
+    def standard_deviation(self):
+        #Odchylenie standardowe dla wybranej kolumny
+
+    def standard_error(self):
+        #Błąd standardowy dla wybranej kolmny
+
+    def __del__(self):
+
