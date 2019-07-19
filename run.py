851dcdc237deaa2353885c0a3ba888dbec62e17e
Patryk Rogaczewski <pat.rogaczewski@gmail.com>
2019-07-19 19:09:49 +0200

    Added functions to statical_cal.py and create menu in run.py

---
+++ run.py
@@ -0,0 +1,124 @@
+import statistical_cal as st
+
+st.menu()
+print("Which number of column do you chose?")
+choice = int(input())
+if choice == 1:
+    #Wynik dla pierwszej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 2:
+    #Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 3:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 4:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 5:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 6:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 7:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 8:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 9:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 10:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 11:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 12:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 13:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+elif choice == 14:
+    # Wynik dla drugiej kolumny
+    st.average(choice)
+    st.median(choice)
+    st.standard_deviation(choice)
+    st.standard_error(choice)
+    st.variance(choice)
+    st.kurtosis(choice)
+else:
+    print("Error")
+
+
+
+
+
--- statistical_cal.py
+++ statistical_cal.py
@@ -1,33 +1,44 @@
-class DataStat():
-    def __init__(self):
+choice = 0
+def menu():
+    print("1. #age [Age in years]: ")
+    print("2. #sex [1 = male; 0 = female]: ")
+    print("3. #cp [chest pain type]: ")
+    print("4. #trestbps [resting blood pressure in mm Hg on admission to the hospital]: ")
+    print("5. #chol [serum cholestoral in mg/dl]: ")
+    print("6. #fbs [fasting blood sugar > 120 mg/dl 1 = true & 0 = false]: ")
+    print("7. #restecg [resting electrocardiographic results]: ")
+    print("8. #thalach [maximum heart rate achieved]: ")
+    print("9. #exang [exercise induced angina 1 = yes & 0 = noe]: ")
+    print("10. #oldpeak [ST depression induced by exercise relative to rest]: ")
+    print("11. #slope [the slope of the peak exercise ST segment]: ")
+    print("12. #ca [number of major vessels 0-3 colored by fluorosopy]: ")
+    print("13. #thal [3= normal; 6 = fixed defect; 7 = reversable defect]: ")
+    print("14. #target [1 or 0]: ")
 
-    def menu(self):
-        print("1. #age [Age in years]: ")
-        print("2. #sex [1 = male; 0 = female]: ")
-        print("3. #cp [chest pain type]: ")
-        print("4. #trestbps [resting blood pressure in mm Hg on admission to the hospital]: ")
-        print("5. #chol [serum cholestoral in mg/dl]: ")
-        print("6. #fbs [fasting blood sugar > 120 mg/dl 1 = true & 0 = false]: ")
-        print("7. #restecg [resting electrocardiographic results]: ")
-        print("8. #thalach [maximum heart rate achieved]: ")
-        print("9. #exang [exercise induced angina 1 = yes & 0 = noe]: ")
-        print("10. #oldpeak [ST depression induced by exercise relative to rest]: ")
-        print("11. #slope [the slope of the peak exercise ST segment]: ")
-        print("12. #ca [number of major vessels 0-3 colored by fluorosopy]: ")
-        print("13. #thal [3= normal; 6 = fixed defect; 7 = reversable defect]: ")
-        print("14. #target [1 or 0]: ")
+def average(choice):
+    print("Średnia dla wybranej kolumny ")
+    print(choice)
 
-    def average(self):
-        #Średnia dla wybranej kolumny
+def median(choice):
+    print("Mediana dla wybranej kolumny ")
+    print(choice)
 
-    def median(self):
-        #Mediana dla wybranej kolumny
 
-    def standard_deviation(self):
-        #Odchylenie standardowe dla wybranej kolumny
+def standard_deviation(choice):
+    print("Odchylenie standardowe dla wybranej kolumny ")
+    print(choice)
 
-    def standard_error(self):
-        #Błąd standardowy dla wybranej kolmny
 
-    def __del__(self):
+def standard_error(choice):
+    print("Błąd standardowy dla wybranej kolmny ")
+    print(choice)
 
+
+def variance(choice):
+    print("Wariancja dla wybranej kolumny ")
+    print(choice)
+
+
+def kurtosis(choice):
+    print("Kurtoza za wybranej kolumny ")
+    print(choice)
